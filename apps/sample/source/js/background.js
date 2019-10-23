5c6434623b32669aff524d3f621179d2fa42416c
robisaacnz <robisaacnz@users.noreply.github.com>
2019-10-23 20:32:01 +1300

    Added sample app for later copying

---
+++ apps/sample/source/js/background.js
@@ -0,0 +1,66 @@
+var appConfig = {
+    "hostname": "icloud.com",
+    "userAgent": "",
+    "chromeAppWindow": {
+        "id": "embed",
+        "frame": {
+            "type": "chrome",
+            "color": "#8d8d8d"
+        },
+        "innerBounds": {
+            "width": 1180,
+            "height": 900
+        }
+    }
+};
+
+// Listen for the app launching then create the window
+chrome.app.runtime.onLaunched.addListener(function () {
+    runApp();
+});
+
+// Listen for a restart then recreate the window
+chrome.app.runtime.onRestarted.addListener(function () {
+    runApp();
+});
+
+// Create the window
+function runApp() {
+    chrome.app.window.create('html/embed.html', appConfig.chromeAppWindow, onWindowLoaded());
+}
+
+function onWindowLoaded(popup) {
+    return function (win) {
+        // On window loaded event
+        win.contentWindow.onload = function () {
+            // Get webview 
+            var webview = win.contentWindow.document.getElementById('webview');
+
+            // Override default user agent if provided
+            if (appConfig.userAgent) {
+                webview.setUserAgentOverride(appConfig.userAgent);
+            }
+
+            // Sign up for 'permissionrequest' event
+            webview.addEventListener('permissionrequest', function (e) {
+                // Allow all permission requests
+                e.request.allow();
+            });
+
+            // Sign up for 'newwindow' event
+            // Emitted when a target='_blank' link is clicked within the webview
+            webview.addEventListener('newwindow', function (e) {
+                // Parse target window URL to extract hostname
+                var parsedUrl = document.createElement('a');
+                parsedUrl.href = e.targetUrl;
+
+                if (parsedUrl.hostname.includes(appConfig.hostname)) {
+                    return chrome.app.window.create('html/embed.html', { frame: { type: 'chrome' }, innerBounds: appConfig.chromeAppWindow.innerBounds }, onWindowLoaded(e));
+                }
+
+                // Open the link in a new browser tab/window
+                win.contentWindow.open(e.targetUrl);
+            });
+        };
+    };
+}
